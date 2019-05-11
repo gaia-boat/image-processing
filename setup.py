68bde01aeca0815ebd70e11cf85bcbd5f2017594
Andre Filho <andre.filho001@outlook.com>
2019-05-10 22:14:32 -0300

    INIT: initial files and folder structure

---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+import codecs
+from os import path
+from setuptools import setup
+from setuptools import find_packages
+from pip._internal.req import parse_requirements
+
+here = path.abspath(path.dirname(__file__))
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allowes setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
+
+# reqs is a list of requirements
+REQUIREMENTS = [str(ir.req) for ir in INSTALL_REQS]
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Everyone",
+    "Natural Language :: Portuguese",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+setup(  # pragma: no cover
+    name='',
+    description="",  # nopep8
+    url='',
+    long_description=codecs.open('README.md', 'rb', 'utf8').read(),
+    long_description_content_type='text/markdown',
+    author='Gaia boat group',
+    author_email='andre.filho001@outlook.com',
+    version=codecs.open('VERSION.txt', 'rb', 'utf8').read(),
+    packages=find_packages(),
+    keywords=['gaia', 'boat', 'pi2'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'ezissue = ezissue.ezissue:main'
+    #     ]
+    # },
+    install_requires=REQUIREMENTS,
+    license='GNU',
+    classifiers=CLASSIFIERS,
+)
