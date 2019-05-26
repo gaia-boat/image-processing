31a927ac450d5655ddc8e7f33be12bf90fb65190
matheusbsilva <matheusbattista@hotmail.com>
2019-05-26 14:15:49 -0300

    add: change project setup to cpp

---
+++ image_processing.cpp
@@ -0,0 +1,39 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#define IMAGE_PATH "./data/trash_on_water.jpg"
+
+using namespace cv;
+
+
+int main(int argc, char **argv) {
+    String image_name(IMAGE_PATH);
+
+    Mat image;
+    image = imread(image_name, 0);
+    resize(image, image, cv::Size(), 0.25, 0.25);
+
+    int height = image.size().height;
+    int width = image.size().width;
+
+    double sum_pixels = 0.0;
+    for(int i = 0; i < image.rows; i++) {
+        for(int j = 0; j < image.cols; j++) {
+            printf("%d\n", (int)image.at<uchar>(i, j));
+            sum_pixels += (int)image.at<uchar>(i, j);
+        }
+    }
+
+    double avg_pixels = sum_pixels / (height * width);
+
+    printf("Avg of pixels: %lf \n", avg_pixels);
+
+    imshow("Output", image);
+
+    waitKey(0);
+
+    return 0;
+
+}
