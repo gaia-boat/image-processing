ec8461da3c6ba0dd37824ec36c9c197c01e8989a
matheusbsilva <matheusbattista@hotmail.com>
2019-06-30 13:24:42 -0300

    Get images from camera and refact functions

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -14,102 +14,137 @@
 using namespace cv;
 using namespace std;
 
+void blur(Mat &image) 
+{
+    Size k(13, 13);
+    GaussianBlur(image, image, k, 0);
+}
 
-void capture() {
+void canny(Mat &image) 
+{
+    double threshold1 = 50;
+    double threshold2 = 100;
+    Canny(image, image, threshold1, threshold2);
+}
 
-    VideoCapture cap;
-    // open the default camera, use something different from 0 otherwise;
-    // Check VideoCapture documentation.
-    if(!cap.open(2))
-        exit(1);
-    for(;;)
+void dilation(Mat &image) 
+{
+    Mat element = getStructuringElement( MORPH_RECT,
+            Size( 3, 3 ));
+    dilate(image, image, element);
+}
+
+void erosion(Mat &image) 
+{
+    int erosion_size = 1;
+    Mat element = getStructuringElement( MORPH_RECT,
+            Size( 3, 3 ));
+    erode(image, image, element);
+
+}
+
+void get_rotated_recs(vector<RotatedRect> &minRect, vector<vector<Point>> &contours)
+{
+    for(int i = 0; i < contours.size(); i++)
     {
-          Mat frame;
-          Mat orgFrame;
-          Mat dest;
-          cap.read(frame);
+        minRect[i] = minAreaRect(Mat(contours[i]));
+    }
+}
 
-          if(frame.empty()) 
-              break; // end of video stream
+void get_contours(Mat &image, vector<vector<Point>> &contours, vector<Vec4i> hierarchy)
+{
+    printf("Finding contours\n");
+    findContours(image, contours, hierarchy,
+            RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+}
 
-          Size img_size(IMAGE_WIDTH, IMAGE_HEIGHT);
-          resize(frame, frame, img_size);
+void draw_objects(Mat &dest_image, vector<vector<Point>> &contours, vector<RotatedRect> &minRect) 
+{
+    RNG rng(63);
 
-          orgFrame = frame.clone();
+    for(int i = 0; i < contours.size(); i++) 
+    {
+        /* Generate random colour for each contour and its rectangle */
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 
-          /* GrayScale */
-          cvtColor(frame, frame, COLOR_BGR2GRAY);
+        /* Draw the contour on the output image */
+        drawContours(dest_image, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point());
 
+        Size2f recSize = Size2f();
+        recSize = minRect[i].size;
 
-          /* Blur */
-          Size k(13, 13);
-          GaussianBlur(frame, frame, k, 0);
+        printf("Height: %lf\n", recSize.height);
+        printf("Width: %lf\n", recSize.width);
 
-          /* Canny */
-          double threshold1 = 50;
-          double threshold2 = 100;
-          Canny(frame, frame, threshold1, threshold2);
+        /* Draw the rectangle */
+        Point2f rect_points[4];
+        minRect[i].points( rect_points );
 
-          /* Dilatation */
-          Mat element = getStructuringElement( MORPH_RECT,
-                  Size( 3, 3 ));
-          dilate(frame, frame, element);
-
-          /* Erosion */
-          int erosion_size = 1;
-          erode(frame, frame, element);
-
-          RNG rng(63);
-
-          /* Get contours */
-          vector<vector<Point>> contours;
-          vector<Vec4i> hierarchy;
-
-          printf("Finding contours\n");
-          findContours(frame, contours, hierarchy,
-                  RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-
-          /* Get the rotated rectangle of each contour */
-          vector<RotatedRect> minRect(contours.size());
-          for(int i = 0; i < contours.size(); i++)
-          {
-              minRect[i] = minAreaRect(Mat(contours[i]));
-          }
-
-          /* Image to draw the contours and rectangles */
-          Mat drawing = Mat::zeros( frame.size(), CV_8UC3 );
-
-          for(int i = 0; i < contours.size(); i++) {
-              /* Generate random colour for each contour and its rectangle */
-              Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-
-              /* Draw the contour on the output image */
-              drawContours(orgFrame, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point());
-
-              Size2f recSize = Size2f();
-              recSize = minRect[i].size;
-
-              printf("Height: %lf\n", recSize.height);
-              printf("Width: %lf\n", recSize.width);
-
-              /* Draw the rectangle */
-              Point2f rect_points[4];
-              minRect[i].points( rect_points );
-
-              for( int j = 0; j < 4; j++ )
-                  line( orgFrame, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
-          }
-
-
-          imshow("this is you, smile! :)", orgFrame);
-          /* imshow("this is you, smile! :)", frame); */
-          if(waitKey(10) == 27)
-              break; // stop capturing by pressing ESC 
+        for( int j = 0; j < 4; j++ )
+            line( dest_image, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
     }
 
-    // the camera will be closed automatically upon exit
-     cap.close();
+}
+
+void capture()
+{
+
+    VideoCapture cap;
+
+    if(!cap.open(2))
+        exit(1);
+
+    for(;;)
+    {
+        Mat frame;
+        Mat orgFrame;
+        Mat dest;
+        cap.read(frame);
+
+        if(frame.empty()) 
+            break; 
+
+        Size img_size(IMAGE_WIDTH, IMAGE_HEIGHT);
+        resize(frame, frame, img_size);
+
+        orgFrame = frame.clone();
+
+        /* GrayScale */
+        cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+        /* Blur */
+        blur(frame);
+
+        /* Canny */
+        canny(frame);
+
+        /* Dilatation */
+        dilation(frame);
+
+        /* Erosion */
+        erosion(frame);
+
+        /* Get contours */
+        vector<vector<Point>> contours;
+        vector<Vec4i> hierarchy;
+        get_contours(frame, contours, hierarchy);
+
+        /* Get the rotated rectangle of each contour */
+        vector<RotatedRect> minRect(contours.size());
+        get_rotated_recs(minRect, contours);
+
+        /* Image to draw the contours and rectangles */
+        Mat drawing = Mat::zeros( frame.size(), CV_8UC3 );
+
+        draw_objects(orgFrame, contours, minRect);
+        draw_objects(drawing, contours, minRect);
+
+        imshow("Original Image", orgFrame);
+        imshow("Contours", drawing);
+
+        if(waitKey(10) == 27)
+            break; // stop capturing by pressing ESC 
+    }
 }
 
 
