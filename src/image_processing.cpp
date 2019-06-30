9f5ae1c41c3737026fbf9b53e59cb42fb69894ee
matheusbsilva <matheusbattista@hotmail.com>
2019-06-30 15:45:37 -0300

    Create routine to check if there's obstacles

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -1,40 +1,42 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
-#include <string>
-#include <iostream>
-#include <fstream>
 
 #define IMAGE_PATH "./data/webcam.jpg"
-/* #define IMAGE_PATH "./data/trash_on_water.jpg" */
 #define OUTPUT_IMAG "./data/one_trash_cont.jpg"
-#define IMAGE_HEIGHT 560 
-#define IMAGE_WIDTH 720 
+#define IMAGE_HEIGHT 560
+#define IMAGE_WIDTH 720
 #define MAX_PIXEL_VAL 255
+
+#define HEIGHT_THRESHOLD 50
+#define WIDTH_THRESHOLD 50
+
+// This must be a float
+#define CM_PER_PIXEL 1.0
 
 using namespace cv;
 using namespace std;
 
-void blur(Mat &image) 
+void blur(Mat &image)
 {
     Size k(13, 13);
     GaussianBlur(image, image, k, 0);
 }
 
-void canny(Mat &image) 
+void canny(Mat &image)
 {
     double threshold1 = 50;
     double threshold2 = 100;
     Canny(image, image, threshold1, threshold2);
 }
 
-void dilation(Mat &image) 
+void dilation(Mat &image)
 {
     Mat element = getStructuringElement( MORPH_RECT,
             Size( 3, 3 ));
     dilate(image, image, element);
 }
 
-void erosion(Mat &image) 
+void erosion(Mat &image)
 {
     int erosion_size = 1;
     Mat element = getStructuringElement( MORPH_RECT,
@@ -53,16 +55,15 @@ void get_rotated_recs(vector<RotatedRect> &minRect, vector<vector<Point>> &conto
 
 void get_contours(Mat &image, vector<vector<Point>> &contours, vector<Vec4i> hierarchy)
 {
-    printf("Finding contours\n");
     findContours(image, contours, hierarchy,
             RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
 }
 
-void draw_objects(Mat &dest_image, vector<vector<Point>> &contours, vector<RotatedRect> &minRect) 
+void draw_objects(Mat &dest_image, vector<vector<Point>> &contours, vector<RotatedRect> &minRect)
 {
     RNG rng(63);
 
-    for(int i = 0; i < contours.size(); i++) 
+    for(int i = 0; i < contours.size(); i++)
     {
         /* Generate random colour for each contour and its rectangle */
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
@@ -72,9 +73,6 @@ void draw_objects(Mat &dest_image, vector<vector<Point>> &contours, vector<Rotat
 
         Size2f recSize = Size2f();
         recSize = minRect[i].size;
-
-        printf("Height: %lf\n", recSize.height);
-        printf("Width: %lf\n", recSize.width);
 
         /* Draw the rectangle */
         Point2f rect_points[4];
@@ -86,7 +84,34 @@ void draw_objects(Mat &dest_image, vector<vector<Point>> &contours, vector<Rotat
 
 }
 
-void capture()
+double pixel_to_cm(double pixel)
+{
+
+    return pixel/CM_PER_PIXEL;
+}
+
+int check_for_obstacle(vector<vector<Point>> &contours, vector<RotatedRect> &minRect)
+{
+    for(int i = 0; i < contours.size(); i++)
+    {
+        Size2f recSize = Size2f();
+        recSize = minRect[i].size;
+
+        double px_height = pixel_to_cm(recSize.height);
+        double px_width = pixel_to_cm(recSize.width);
+
+        if(px_height >= HEIGHT_THRESHOLD || px_height >= WIDTH_THRESHOLD )
+        {
+            printf("\n\n>>>>>> Obstacle detected! <<<<<<\n");
+            printf(">>>>>> Obstacle height: %.2lf cm <<<<<<\n", px_height);
+            printf(">>>>>> Obstacle witdh:  %.2lf cm <<<<<<\n", px_width);
+            return 1;
+        }
+        return 0;
+    }
+}
+
+int capture()
 {
 
     VideoCapture cap;
@@ -94,15 +119,15 @@ void capture()
     if(!cap.open(2))
         exit(1);
 
-    for(;;)
+    for(int i = 0; i < 10; i++)
     {
         Mat frame;
         Mat orgFrame;
         Mat dest;
         cap.read(frame);
 
-        if(frame.empty()) 
-            break; 
+        if(frame.empty())
+            break;
 
         Size img_size(IMAGE_WIDTH, IMAGE_HEIGHT);
         resize(frame, frame, img_size);
@@ -138,16 +163,29 @@ void capture()
 
         draw_objects(orgFrame, contours, minRect);
         draw_objects(drawing, contours, minRect);
+        int obs = 0;
+
+        obs = check_for_obstacle(contours, minRect);
+
+        if(obs){
+            return obs;
+        }
+
 
         imshow("Original Image", orgFrame);
         imshow("Contours", drawing);
 
-        if(waitKey(10) == 27)
-            break; // stop capturing by pressing ESC 
+        waitKey(0);
+        /* if(waitKey(10) == 27) */
+        /*     break; // stop capturing by pressing ESC */
     }
+
+    return 0;
 }
 
 
 int main() {
-    capture();
+    if(capture())
+        return 1;
+
 }
