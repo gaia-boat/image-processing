7dab7a4d1fc39f583191f377759509fd5974c8b8
matheusbsilva <matheusbattista@hotmail.com>
2019-07-02 22:24:35 -0300

    Improve opencv installation script

--- dependecies/opencv.sh
+++ dependecies/opencv.sh
@@ -1,19 +1,59 @@
-# apt-get install build-essential
+#!/bin/bash
 
-# apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+set -e
 
-# apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+if [ "$EUID" -ne 0 ] 
+then
+    echo "Please run as root"
+    exit
+fi
 
-# git clone https://github.com/opencv/opencv.git /tmp/opencv
-# git clone https://github.com/opencv/opencv_contrib.git /tmp/opencv_contrib
+DIR=$HOME
+OPENCV_DIR=$DIR/opencv/opencv
+OPENCV_CONTRIB_DIR=$DIR/opencv/opencv_contrib
+OPENCV_BUILD_DIR=$OPENCV_DIR/build
+OPENCV_VERSION=4.1.0
 
-# git -C /tmp/opencv checkout tags/4.1.0 -b 4.1.0
-# git -C /tmp/opencv_contrib checkout tags/4.1.0 -b 4.1.0
+PYTHON3_EXEC_DIR=/usr/bin/python3
+PYTHON3_INCLUDE_DIR=/usr/include/python3.6
+PYTHON3_LIB_DIR=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
 
-mkdir -p /tmp/opencv/build
+INSTALL_PREFIX_DIR=/usr/local
 
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/tmp/opencv_contrib/modules -D PYTHON3_EXECUTABLE=/usr/bin/python3 -D PYTHON_INLCUDE_DIR=/usr/include/python3.6 -D PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so -D OPENCV_GENERATE_PKGCONFIG=ON -B/tmp/opencv/build -H/tmp/opencv
+echo "Installing required packages"
+apt-get install -y build-essential \
+                cmake git libgtk2.0-dev \
+                pkg-config libavcodec-dev \
+                libavformat-dev libswscale-dev
 
-make -j7 -C /tmp/opencv/build
+apt-get install -y python-dev python-numpy \
+                libtbb2 libtbb-dev libjpeg-dev \
+                libpng-dev libtiff-dev \
+                libjasper-dev libdc1394-22-dev
 
-make install -C /tmp/opencv/build
+echo "Clonning opencv core repository"
+git clone https://github.com/opencv/opencv.git $OPENCV_DIR
+
+echo "Clonning opencv contrib core repository"
+git clone https://github.com/opencv/opencv_contrib.git $OPENCV_CONTRIB_DIR
+
+echo "Selecting Version $OPENCV_VERSION"
+git -C $OPENCV_DIR checkout tags/$OPENCV_VERSION -b $OPENCV_VERSION
+git -C $OPENCV_CONTRIB_DIR checkout tags/$OPENCV_VERSION -b $OPENCV_VERSION
+
+echo "Creating build dir"
+mkdir -p $OPENCV_BUILD_DIR
+
+echo "Building Opencv..."
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$INSTALL_PREFIX_DIR \
+      -D OPENCV_EXTRA_MODULES_PATH=$OPENCV_CONTRIB_DIR/modules \
+      -D PYTHON3_EXECUTABLE=$PYTHON3_EXEC_DIR \
+      -D PYTHON_INLCUDE_DIR=$PYTHON3_INCLUDE_DIR \
+      -D PYTHON_LIBRARY=$PYTHON3_LIB_DIR \
+      -D OPENCV_GENERATE_PKGCONFIG=ON -B$OPENCV_BUILD_DIR -H$OPENCV_DIR
+
+echo "Running make..."
+make -j7 -C $OPENCV_BUILD_DIR
+
+echo "Installing OpenCV"
+make install -C $OPENCV_BUILD_DIR 
